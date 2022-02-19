f34beca4dc621cf8b0590013e4130135e5448c54
Tom <tom@contextfree.info>
2022-02-19 08:19:47 -0700

    Format and think on pattens

--- boolerr.ts
+++ boolerr.ts
@@ -1,34 +1,39 @@
+type Char = string;
+type Repeat = { repeat: Char };
+type Part = Char | Repeat;
+type Pattern = Part[];
+
 function parseBool(text: string): boolean {
-    const result = {true: true, false: false}[text];
-    if (result == null) {
-        throw Error("bad bool")
-    }
-    return result;
+  const result = { true: true, false: false }[text];
+  if (result == null) {
+    throw Error("bad bool");
+  }
+  return result;
 }
 
 function parseBoolResult(text: string): boolean | Error {
-    const result = {true: true, false: false}[text];
-    if (result == null) {
-        return Error("bad bool")
-    }
-    return result;
+  const result = { true: true, false: false }[text];
+  if (result == null) {
+    return Error("bad bool");
+  }
+  return result;
 }
 
 function process(text: string): boolean | undefined {
-    return text ? parseBool(text) : undefined;
+  return text ? parseBool(text) : undefined;
 }
 
 // function processResult(text: string): undefined | true | Error | false {
 function processResult(text: string): boolean | Error | undefined {
-    return text ? parseBoolResult(text) : undefined;
+  return text ? parseBoolResult(text) : undefined;
 }
 
 function main() {
-    for (let text of ["true", "false", "", "bad"]) {
-        const processed = processResult(text);
-        const truthy = processed ? "✓" : "✗";
-        console.log(`${truthy} "${text}" is ${processed}`)
-    }
+  for (let text of ["true", "false", "", "bad"]) {
+    const processed = processResult(text);
+    const truthy = processed ? "✓" : "✗";
+    console.log(`${truthy} "${text}" is ${processed}`);
+  }
 }
 
-main()
+main();
