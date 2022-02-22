0173ff8cb5025452cb276f36c5d8ab5c9bc4a84e
Tom <tom@contextfree.info>
2022-02-21 20:48:42 -0700

    Start parse

--- boolerr.rs
+++ boolerr.rs
@@ -1,3 +1,36 @@
+use std::str::Chars;
+
+enum Part {
+    Letter(char),
+    Repeat(char),
+}
+
+type Pattern = Vec<Part>;
+
+struct PartSource<'a> {
+    part: Part,
+    source: Chars<'a>,
+}
+
+#[derive(Debug)]
+struct ParseError {}
+impl std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+impl std::error::Error for ParseError {}
+
+fn parse_letter(mut source: Chars) -> Result<PartSource, ParseError> {
+    match source.next() {
+        Some(letter @ 'a'..='z') => Ok(PartSource {
+            part: Part::Letter(letter),
+            source,
+        }),
+        _ => Err(ParseError {}),
+    }
+}
+
 fn main() {
     //
 }
