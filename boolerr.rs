8757fd52ac80e0b38c65875ab143ee5ebdf27177
Tom <tom@contextfree.info>
2022-02-21 21:42:11 -0700

    Parse patterns

--- boolerr.rs
+++ boolerr.rs
@@ -1,11 +1,18 @@
+use std::iter::Peekable;
 use std::str::Chars;
 
+#[derive(Debug)]
+struct Letter(char);
+#[derive(Debug)]
+struct Repeat(Letter);
+#[derive(Debug)]
 enum Part {
-    Letter(char),
-    Repeat(char),
+    Letter(Letter),
+    Repeat(Repeat),
 }
-
 type Pattern = Vec<Part>;
+
+type Source<'a> = Peekable<Chars<'a>>;
 
 #[derive(Debug)]
 struct ParseError {}
@@ -16,13 +23,37 @@ impl std::fmt::Display for ParseError {
 }
 impl std::error::Error for ParseError {}
 
-fn parse_letter(source: &mut Chars) -> Result<Part, ParseError> {
-    match source.next() {
-        Some(letter @ 'a'..='z') => Ok(Part::Letter(letter)),
+fn parse_letter(source: &mut Source) -> Result<Letter, ParseError> {
+    match source.peek() {
+        Some(&c @ 'a'..='z') => {
+            source.next();
+            Ok(Letter(c))
+        }
         _ => Err(ParseError {}),
     }
 }
 
+fn parse_repeat(source: &mut Source) -> Result<Part, ParseError> {
+    let sub = parse_letter(source)?;
+    match source.peek() {
+        Some('*') => {
+            source.next();
+            Ok(Part::Repeat(Repeat(sub)))
+        }
+        _ => Ok(Part::Letter(sub)),
+    }
+}
+
+fn parse_pattern(source: &mut Source) -> Result<Pattern, ParseError> {
+    let mut pattern = vec![];
+    while source.peek().is_some() {
+        let next = parse_repeat(source)?;
+        pattern.push(next);
+    }
+    Ok(pattern)
+}
+
 fn main() {
-    //
+    let pattern = parse_pattern(&mut "a*b".chars().peekable());
+    println!("{:?}", pattern);
 }
