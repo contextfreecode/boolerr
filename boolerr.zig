c1768dc8f3bb323629a0d0121e98aa1f94880439
Tom <tom@contextfree.info>
2022-02-26 07:04:26 -0700

    Make progress on zig

--- boolerr.zig
+++ boolerr.zig
@@ -1,8 +1,12 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
 const Doc = struct { head: ?Head };
-
 const Head = struct { title: ?[]u8 };
+const DocReport = struct {
+    title: ?[]u8,
+    ok: bool,
+};
 
 const Error = error{FailedRead};
 
@@ -10,7 +14,7 @@ fn contains(comptime T: type, haystack: []const T, needle: []const T) bool {
     return std.mem.indexOf(u8, haystack, needle) != null;
 }
 
-fn readDoc(url: []const u8) !Doc {
+fn readDoc(allocator: Allocator, url: []const u8) !Doc {
     return if (contains(u8, url, "fail"))
         error.FailedRead
     else if (contains(u8, url, "headless"))
@@ -18,12 +22,33 @@ fn readDoc(url: []const u8) !Doc {
     else if (contains(u8, url, "empty"))
         Doc{ .head = Head{ .title = null } }
     else
-        Doc{ .head = null };
+        Doc{ .head = Head{ .title = try std.fmt.allocPrint(allocator, "Something", .{}) } };
 }
 
-pub fn main() void {
+fn buildDocReport(doc: Doc) DocReport {
+    return DocReport{
+        .title = if (doc.head) |head| head.title else null,
+        .ok = true,
+    };
+}
+
+fn readAndBuildDocReport(allocator: Allocator, url: []const u8) DocReport {
+    const doc = readDoc(allocator, url) catch {
+        return DocReport{ .title = null, .ok = false };
+    };
+    return buildDocReport(doc);
+}
+
+pub fn main() !void {
+    const print = std.io.getStdOut().writer().print;
     const urls = [_][]const u8{ "good", "empty", "headless", "fail" };
     for (urls) |url| {
-        _ = readDoc(url) catch void;
+        // Treat each scrape independently for memory.
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer arena.deinit();
+        const allocator = arena.allocator();
+        // Scrape.
+        try print("Checking \"https://{s}/\":\n", .{url});
+        try print("  Report: {}\n", .{readAndBuildDocReport(allocator, url)});
     }
 }
